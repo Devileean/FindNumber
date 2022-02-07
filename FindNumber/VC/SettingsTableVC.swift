21e702e33a7ab902021bee63b8b28a0a39ce0659
Alexsey Loginov <aleniklog@gmail.com>
2022-02-08 01:34:54 +0300

    add logic for time and switch

--- FindNumber/VC/SettingsTableVC.swift
+++ FindNumber/VC/SettingsTableVC.swift
@@ -9,12 +9,27 @@ import UIKit
 
 class SettingsTableVC: UITableViewController {
 
+    @IBOutlet weak var switchTimer: UISwitch!
+    @IBOutlet weak var timeGameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadSettings()
+    }
     
+    @IBAction func changeTimerState(_ sender: UISwitch) {
+        Settings.shared.currentSettings.timeState = sender.isOn
+    }
+    
+    func loadSettings() {
+        timeGameLabel.text = "\(Settings.shared.currentSettings.timeForGame) сек"
+        switchTimer.isOn = Settings.shared.currentSettings.timeState
+    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         switch segue.identifier {
         case "selectTimeVC" :
